ff7746a43364bcf6cbf5bca35d8f1516a72f905b
Manuel Alabor <manuel@alabor.me>
2022-01-11 08:12:20 +0100

    Add CSS Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Board from "./components/Board";
 import boardFixture from "./model/fixture/board";
+import { Reset } from "styled-reset";
 
 function App() {
   const [board, setBoard] = React.useState(boardFixture);
-  return <Board board={board} onChange={setBoard} />;
+  return (
+    <>
+      <Reset />
+      <Board board={board} onChange={setBoard} />
+    </>
+  );
 }
 
 export default App;
